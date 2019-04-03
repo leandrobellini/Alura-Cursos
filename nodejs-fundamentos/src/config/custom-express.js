1fbb52d27d3b42c31d374c020ca2b1d4101f0ad7
leandrobellini <leandroluisbellini@gmail.com>
2019-04-02 23:02:16 -0300

    Mais ajustes

--- nodejs-fundamentos/src/config/custom-express.js
+++ nodejs-fundamentos/src/config/custom-express.js
@@ -2,11 +2,24 @@ const express   = require('express');
 const app       = express();
 const routes    = require('../app/routes/routes');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({
     extended: true
 }
 ));
+
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }));
 
 routes(app);
 
